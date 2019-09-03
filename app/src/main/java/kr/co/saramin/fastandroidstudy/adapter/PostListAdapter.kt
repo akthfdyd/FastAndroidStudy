7e48b67ca960a0d66884dd8074fb125339061fc7
akthfdyd <akthfdyd@gmail.com>
2019-09-03 13:51:25 +0900

    glide crop circle

--- app/src/main/java/kr/co/saramin/fastandroidstudy/adapter/PostListAdapter.kt
+++ app/src/main/java/kr/co/saramin/fastandroidstudy/adapter/PostListAdapter.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import kr.co.saramin.fastandroidstudy.R
 import kr.co.saramin.fastandroidstudy.WebViewActivity
 import kr.co.saramin.fastandroidstudy.vo.BlogPostResponseModel
@@ -63,6 +64,7 @@ class PostListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             Glide.with(itemView)
                 .load(profileImageUrl)
                 .centerCrop()
+                .apply(RequestOptions.circleCropTransform())
                 .into(profileImage)
 
             container.setOnClickListener {
